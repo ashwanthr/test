b71e288dd1f376efc95d5ccf9f312dac871450a0
Ashwanth K Salibindla <AKS5577@coeaccess.psu.edu>
2018-01-14 19:27:21 -0500

    added subtraction

--- arithmetic.m
+++ arithmetic.m
@@ -4,6 +4,8 @@ b = 10;
 c = a + b;
 d = a*b;
 e = a/b;
+f = a-b;
 fprintf("Sum is: %5.5f \n", c);
 fprintf("Product is: %5.5f \n", d);
 fprintf("Ratio is: %5.5f \n", e);
+fprintf("Difference is: %5.5f \n", f);
